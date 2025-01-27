e4e8ba512a7abb95ef6976acdfed77ce0b43257c
billn <billnyetherussianspy679@gmail.com>
2025-01-27 19:29:43 +0300

    saving changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+      <div>
+        <h1>Hi, I'm (your name)</h1>
+        <img src='../public/images/logo.png' alt='A picture of myself' />
+        <h2>About Me</h2>
+        <p>My name is Tamara and I am a junior developer.</p>
+        <a href='https://github.com/tammywantstodev'>Github</a>
+        <a href='https://linkedin.com/tammywantstodev'>LinkedIn</a>
+      </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,4 +3,37 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    });
+
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+
+    const imageOfMyself = screen.getByRole("img", { name: /picture of myself/i });
+    expect(imageOfMyself).toBeInTheDocument();
+
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /about me/i,
+        exact: false,
+        level: 2,
+    });
+    const paragraph=document.querySelector("p")
+    expect(paragraph).toBeInTheDocument();
+
+    const myGithubLink = document.querySelector("a");
+    expect(myGithubLink).toBeInTheDocument();
+    expect(myGithubLink).toHaveAttribute("href", "https://github.com/tammywantstodev");
+
+    const myLinkedInLink=document.querySelector("a");
+    expect(myLinkedInLink).toBeInTheDocument();
+    expect(myGithubLink).toHaveAttribute("href", "https://github.com/tammywantstodev");
+
+});
